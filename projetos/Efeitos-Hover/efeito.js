9a15f3247bc704a6c2bf5680b9d661e9226cbf7c
Geovanne19 <130259323+Geovanne19@users.noreply.github.com>
2023-09-29 12:55:48 -0300

    .

---
+++ projetos/Efeitos-Hover/efeito.js
@@ -0,0 +1,19 @@
+var btns = document.querySelectorAll/*estudar */('.btn');
+
+btns.forEach/*estudar */(function(btn) {
+    btn.onmousemove = function(e){
+        var x = e.pageX - btn.offsetLeft/*estudar */;
+        var y = e.pageY - btn.offsetTop;
+
+        btn.style.setProperty('--eixoX', x + 'px');
+        btn.style.setProperty('--eixoY', y + 'px');
+    }
+});
+
+let trilho = document.getElementById('trilho')
+let body = document.querySelector('body')
+
+trilho.addEventListener('click', ()=>{
+    trilho.classList.toggle('dark')
+    body.classList/*estudar */.toggle('dark')
+})
